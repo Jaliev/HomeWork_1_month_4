5ee0d7dba66a7b6b8fcf036a49df0b31acf6ecee
Jaliev <jaliev.arslanbek.aibekovich@gmail.com>
2023-10-20 14:28:17 +0600

    HW3, Добавил CRUD, сделал всё как показывали на уроке

--- auto_parts_store/urls.py
+++ auto_parts_store/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('avtoparts_list/', views.avtopartsListView, name='AvtopartsList'),
     path('avtoparts_detail/<int:id>/', views.avtopartsDetailView, name='detail'),
+    path('avtoparts_detail/<int:id>/delete/', views.deleteProductsView, name='delete'),
+    path('avtoparts_detail/<int:id>/update/', views.updateProductsView, name='update'),
+    path('create_product/', views.createProductsView, name='createProduct'),
 ]
--- auto_parts_store/views.py
+++ auto_parts_store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from . import models
+from django.http import HttpResponse
+from . import models, forms
 
 def avtopartsListView(request):
     avtoparts = models.AutoParts.objects.all()
@@ -16,3 +17,37 @@ def avtopartsDetailView(request, id):
         'avtoparts_id': avtoparts_id,
     }
     return render(request, html_name, context)
+
+def createProductsView(request):
+    method = request.method
+    if method == "POST":
+        form = forms.ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Товар успешно добавлен')
+    else:
+        form = forms.ProductForm()
+
+    return render(request, 'avto_parts/create_product.html', {'form': form})
+
+def deleteProductsView(request, id):
+    product_id = get_object_or_404(models.AutoParts, id=id)
+    product_id.delete()
+    return HttpResponse('Товар успешно удалён')
+
+def updateProductsView(request, id):
+    product_id = get_object_or_404(models.AutoParts, id=id)
+    if request.method == 'POST':
+        form = forms.ProductForm(instance=product_id, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Товар изменён')
+
+    else:
+        form = forms.ProductForm(instance=product_id)
+    return render(request, 'avto_parts/update_product.html',
+                  {
+                      'form': form,
+                      'product_id': product_id
+                  }
+                  )
